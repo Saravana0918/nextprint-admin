d8f9b2c4c8f2cc9ae4dca3da544b36fa32cb2074
Saravana Kumar <saravananextprint@gmail.com>
2025-10-13 18:20:38 +0530

    DESIGNING TOOL IMAGE FUNCTION SETTINGS

--- app/Http/Controllers/ProductPreviewController.php
+++ app/Http/Controllers/ProductPreviewController.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
